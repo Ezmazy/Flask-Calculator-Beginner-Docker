a5ddb3c446242accb6acc3b2610bc8ab9b23ba0a
Ezmazy <ezmazyhd@gmail.com>
2026-05-19 15:10:56 +0300

    edited app.py , templates

--- app.py
+++ app.py
@@ -1,21 +1,18 @@
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, render_template
 from flask_restful import Api, Resource
 from logic import Add, Subtract, Multiply, Divide
-
 
 app = Flask(__name__)
 api = Api(app)
 
-api.add_resource(Add, "/add")
+api.add_resource(Add,      "/add")
 api.add_resource(Subtract, "/subtract")
 api.add_resource(Multiply, "/multiply")
-api.add_resource(Divide, "/division")
-
+api.add_resource(Divide,   "/division")
 
 @app.route('/')
-def hello_world():
-    return "Hello World!"
+def index():
+    return render_template('index.html')
 
-
-if __name__=="__main__":
+if __name__ == "__main__":
     app.run(host="0.0.0.0")
